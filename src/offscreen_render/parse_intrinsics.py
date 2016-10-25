8491ffbcfc39492c0a37175783456770a6404933
Rachel <rmh@andrew.cmu.edu>
2016-10-25 13:09:53 -0400

    Remove testing from parsing and add doc string

--- src/offscreen_render/parse_intrinsics.py
+++ src/offscreen_render/parse_intrinsics.py
@@ -3,12 +3,16 @@ import numpy as np
 import yaml
 
 def getIntrinsics(filepath):
-	camera = np.array( cv.Load(filepath, cv.CreateMemStorage(), "cameraMatrix") )
-	fx = camera[0][0]
-	fy = camera[1][1]
-	cx = camera[0][2]
-	cy = camera[1][2]
-	print "fx", fx
-	return fx, fy, cx, cy
-filepath = '/home/pengjuj/ros/local/catkin_ws/src/herb_launch/calibration/kinect2/502845441942/calib_color.yaml'
-getIntrinsics(filepath)	
+    """
+    Given a file path to camera intrinsics, parse
+    out the parameters.
+
+    @param filepath Path to read
+    @return fx, fy, cx, cy
+    """
+    camera = np.array( cv.Load(filepath, cv.CreateMemStorage(), "cameraMatrix") )
+    fx = camera[0][0]
+    fy = camera[1][1]
+    cx = camera[0][2]
+    cy = camera[1][2]
+    return fx, fy, cx, cy
